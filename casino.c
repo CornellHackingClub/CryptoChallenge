58e5f6fcc763232133e0034b7856bcc054d71861
Jimmy Cao <jc2462@cornell.edu>
2016-11-17 01:41:30 -0500

    Oops, avoid buffer overflow.

--- casino.c
+++ casino.c
@@ -11,13 +11,17 @@ int main(int argc, char *argv[]) {
     char buf[128];
 	if(argc < 2) {
 		printf("Usage: %s <plaintext>\n", argv[0]);
-		exit(0);
+		exit(1);
 	}
     char padding[17];
     memset(padding, 'X', sizeof(padding));
     padding[16 - ( strlen(argv[1]) % 16 )] = '\0';
 
     int len = snprintf(buf, 128, "%s%s%s", argv[1], flag, padding);
+    if(len > 127) {
+        printf("Please use a smaller input.\n");
+        exit(1);
+    }
 
     char output[128];
     int i;
